2d3cb3ba294db43d8a1a0a6befd363827f13797b
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-19 12:00:18 -0500

    commit tree 93b267d95af3a62d6ab3d35b768885218644e64a

--- main.js
+++ main.js
@@ -11,15 +11,49 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
-        resolve("Pretend remote server notified of action!");
+        resolve("Pretend remote server notified of action!")
       }
     }, 300);
   });
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  addEventListenerToLikes()
+});
+
+
+
+function addEventListenerToLikes() {
+  document.querySelectorAll('.like-glyph').forEach(like => like.addEventListener('click', likeButton))
+  // document.querySelectorAll('.like-glyph').forEach(like => like.addEventListener('click', mimicServerCall))
+
+}
+
+function likeButton(e) {
+  const modal = document.querySelector('#modal')
+  mimicServerCall("weqweqw")
+  .then(function(message) {
+    console.log("yo");
+    if (e.target.classList.value.includes('activated-heart')) {
+      e.target.classList.remove('activated-heart')
+    } else {
+      e.target.classList.add('activated-heart')
+    }
+  })
+    .catch(function (error) {
+      modal.innerHTML = error
+      modal.classList.remove('hidden')
+      setTimeout(function () {
+        modal.classList.add('hidden')
+      }, 5000)
+    })
+  console.log("hi")
 }
